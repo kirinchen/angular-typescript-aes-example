ecf6ebe4a2ad67c7daf1c36253cafd0106981ca5
kirin <kirin.chen1001@gmail.com>
2020-11-29 17:07:28 +0800

    new custom aes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,23 +9,40 @@ import * as CryptoJS from 'crypto-js';
 export class AppComponent {
   title = 'EncryptionDecryptionSample';
 
-  plainText:string;
+  plainText: string;
   encryptText: string;
   encPassword: string;
-  decPassword:string;
+  decPassword: string;
   conversionEncryptOutput: string;
-  conversionDecryptOutput:string;
+  conversionDecryptOutput: string;
 
   constructor() {
   }
-  //method is used to encrypt and decrypt the text
-  convertText(conversion:string) {
-      if (conversion=="encrypt") {
-        this.conversionEncryptOutput = CryptoJS.AES.encrypt(this.plainText.trim(), this.encPassword.trim()).toString();
-      }
-      else {
-        this.conversionDecryptOutput = CryptoJS.AES.decrypt(this.encryptText.trim(), this.decPassword.trim()).toString(CryptoJS.enc.Utf8);
+  // method is used to encrypt and decrypt the text
+  convertText(conversion: string) {
+    if (conversion === 'encrypt') {
+      this.conversionEncryptOutput = CryptoJS.AES.encrypt(this.plainText.trim(), this.encPassword.trim()).toString();
+    }
+    else {
+      this.conversionDecryptOutput = CryptoJS.AES.decrypt(this.encryptText.trim(), this.decPassword.trim()).toString(CryptoJS.enc.Utf8);
 
     }
+  }
+
+  public decryptByCustom(): void {
+    const key = CryptoJS.enc.Utf8.parse(this.decPassword.trim()); // 秘钥
+
+    // const wsk = this.encryptText.trim(); // 后台返回的加密字符串
+    // const encryptedHexStr = CryptoJS.enc.Hex.parse(wsk); // a551e4d54cfc396cc93c89dd55f6587c
+    // const encryptedBase64Str = CryptoJS.enc.Base64.stringify(encryptedHexStr);
+    const encryptedBase64Str = this.encryptText.trim();
+    const decryptedData = CryptoJS.AES.decrypt(encryptedBase64Str, key, {
+      mode: CryptoJS.mode.ECB,
+      padding: CryptoJS.pad.Pkcs7
+    });
+
+    const decryptedStr = decryptedData.toString(CryptoJS.enc.Utf8);
+
+    console.log('解密后:' + decryptedStr);
   }
 }
